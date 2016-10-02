005bcda63c1003cceef9f45c5adb08b95f161f54
btthan <buithithan.cntt@gmail.com>
2016-10-02 17:54:44 +0700

    format project

--- RenterRoom/src/action/RoomListAction.java
+++ RenterRoom/src/action/RoomListAction.java
@@ -19,37 +19,39 @@ import bo.RoomBo;
 @WebServlet("/RoomListAction")
 public class RoomListAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RoomListAction() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public RoomListAction() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServv letResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
-		
+
 		RoomBo list = new RoomBo();
 		ArrayList<Rooms> listRoom = list.getListRoom();
-		
-		request.setAttribute("listRoom",listRoom);
-		RequestDispatcher rd = request.getRequestDispatcher("host/RoomList.jsp");
+
+		request.setAttribute("listRoom", listRoom);
+		RequestDispatcher rd = request
+				.getRequestDispatcher("host/RoomList.jsp");
 		rd.forward(request, response);
 	}
 
--- RenterRoom/src/action/RoomUpdateAction.java
+++ RenterRoom/src/action/RoomUpdateAction.java
@@ -17,41 +17,43 @@ import bo.RoomBo;
 @WebServlet("/RoomUpdateAction")
 public class RoomUpdateAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RoomUpdateAction() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public RoomUpdateAction() {
+		super();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
-		
+
 		int id = Integer.parseInt(request.getParameter("idRoom"));
-		
+
 		System.out.println("Update: " + id);
 		RoomBo roomBo = new RoomBo();
 		Rooms objRoom = new Rooms();
 		objRoom.setIdRoom(id);
-		
+
 		int isEmpty = roomBo.getIsEmpty(id).getIsEmpty();
-		objRoom.setIsEmpty(isEmpty==1 ? 0 : 1);
-		
+		objRoom.setIsEmpty(isEmpty == 1 ? 0 : 1);
+
 		roomBo.editIsEmptyRoom(objRoom);
 
 		response.sendRedirect(request.getContextPath() + "/RoomListAction");
