64f7ae9d59b795b91854c584629c904111a2e024
phuongpham149 <phamthiphuong.cntt@gmail.com>
2016-10-16 12:30:34 +0700

    luu tam

--- RenterRoom/src/bean/Rooms.java
+++ RenterRoom/src/bean/Rooms.java
@@ -1,5 +1,7 @@
 package bean;
 
+import java.sql.Date;
+
 public class Rooms {
 	private int idRoom;
 	private int idUser;
@@ -7,27 +9,30 @@ public class Rooms {
 	private int cost;
 	private String description;
 	private int isActive;
-	private String timeCreated;
-	private String timeUpdate;
+	private Date timeCreated;
+	private Date timeUpdate;
 	private int isEmpty;
-	private String nameRoom ;
+	private String nameRoom;
 	private String image;
 	private String street;
 	private String district;
 	private String nameCategory;
-	
+
 	public String getNameCategory() {
 		return nameCategory;
 	}
+
 	public void setNameCategory(String nameCategory) {
 		this.nameCategory = nameCategory;
 	}
+
 	public Rooms() {
 		super();
 	}
+
 	public Rooms(int idRoom, int idUser, int idCategory, int cost,
-			String description, int isActive, String timeCreated,
-			String timeUpdate, int isEmpty, String nameRoom, String image,
+			String description, int isActive, Date timeCreated,
+			Date timeUpdate, int isEmpty, String nameRoom, String image,
 			String street, String district) {
 		super();
 		this.idRoom = idRoom;
@@ -44,87 +49,114 @@ public class Rooms {
 		this.street = street;
 		this.district = district;
 	}
+
 	public int getIdRoom() {
 		return idRoom;
 	}
+
 	public void setIdRoom(int idRoom) {
 		this.idRoom = idRoom;
 	}
+
 	public int getIdUser() {
 		return idUser;
 	}
+
 	public void setIdUser(int idUser) {
 		this.idUser = idUser;
 	}
+
 	public int getIdCategory() {
 		return idCategory;
 	}
+
 	public void setIdCategory(int idCategory) {
 		this.idCategory = idCategory;
 	}
+
 	public int getCost() {
 		return cost;
 	}
+
 	public void setCost(int cost) {
 		this.cost = cost;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getIsActive() {
 		return isActive;
 	}
+
 	public void setIsActive(int isActive) {
 		this.isActive = isActive;
 	}
-	public String getTimeCreated() {
+
+	public Date getTimeCreated() {
 		return timeCreated;
 	}
-	public void setTimeCreated(String timeCreated) {
+
+	public void setTimeCreated(Date timeCreated) {
 		this.timeCreated = timeCreated;
 	}
-	public String getTimeUpdate() {
+
+	public Date getTimeUpdate() {
 		return timeUpdate;
 	}
-	public void setTimeUpdate(String timeUpdate) {
+
+	public void setTimeUpdate(Date timeUpdate) {
 		this.timeUpdate = timeUpdate;
 	}
+
 	public int getIsEmpty() {
 		return isEmpty;
 	}
+
 	public void setIsEmpty(int isEmpty) {
 		this.isEmpty = isEmpty;
 	}
+
 	public String getNameRoom() {
 		return nameRoom;
 	}
+
 	public void setNameRoom(String nameRoom) {
 		this.nameRoom = nameRoom;
 	}
+
 	public String getImage() {
 		return image;
 	}
+
 	public void setImage(String image) {
 		this.image = image;
 	}
+
 	public String getStreet() {
 		return street;
 	}
+
 	public void setStreet(String street) {
 		this.street = street;
 	}
+
 	public String getDistrict() {
 		return district;
 	}
+
 	public void setDistrict(String district) {
 		this.district = district;
 	}
+
 	public Rooms(int idRoom, int idUser, int idCategory, int cost,
-			String description, int isActive, String timeCreated,
-			String timeUpdate, int isEmpty, String nameRoom, String image,
+			String description, int isActive, Date timeCreated,
+			Date timeUpdate, int isEmpty, String nameRoom, String image,
 			String street, String district, String nameCategory) {
 		super();
 		this.idRoom = idRoom;
@@ -142,6 +174,5 @@ public class Rooms {
 		this.district = district;
 		this.nameCategory = nameCategory;
 	}
-	
 
 }
--- RenterRoom/src/dao/Database.java
+++ RenterRoom/src/dao/Database.java
@@ -1,26 +1,77 @@
 package dao;
 
-import java.io.PrintWriter;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
 
 public class Database {
-	public Connection connectDB(){
-		Connection cnn=null;
-		
+	private static Connection con = null;
+
+	public static Connection connectDB() {
 		try {
-			//nạp driver
-			Class.forName("com.mysql.jdbc.Driver");
-			cnn = DriverManager.getConnection("jdbc:mysql://localhost:3306/renterroom?useUnicode=true&characterEncoding=UTF-8", "root", "");
-			
-			
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				System.out.println("Error not find class");
+				e.printStackTrace();
+			}
+
+			String connectionURL = "jdbc:mysql://localhost:3306/renterroom?useUnicode=true&characterEncoding=UTF-8";
+			con = DriverManager.getConnection(connectionURL, "root", "");
+
+		} catch (SQLException e) {
+			System.out.println("Error connect");
 			e.printStackTrace();
 		}
-		return cnn;
+		return con;
+
 	}
+
+	public static void closeConnection(Connection conn) {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.err.println("Can not close connection !");
+			e.printStackTrace();
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			System.err.println("Can not close ResultSet !");
+			e.printStackTrace();
+		}
+	}
+
+	public static void closePrepareStatement(PreparedStatement pstmt) {
+		try {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		} catch (SQLException e) {
+			System.err.println("Can not close PreparedStatement  !");
+			e.printStackTrace();
+		}
+	}
+
+	public static void closeCallableStatement(CallableStatement cstmt) {
+		try {
+			if (cstmt != null) {
+				cstmt.close();
+			}
+		} catch (SQLException e) {
+			System.err.println("Can not close CallableStatement  !");
+			e.printStackTrace();
+		}
+	}
+
 }
